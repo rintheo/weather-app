08b1b2c0015912457685fc477e874cbe930a7597
rintheo <107115767+rintheo@users.noreply.github.com>
2023-08-15 11:00:04 +0800

    Add hourly forecast details when clicking 'See Hourly' button

--- src/index.js
+++ src/index.js
@@ -385,7 +385,20 @@ const generateHourlyForecastCard = (response) => {
   button.classList.add('details-button');
   button.textContent = 'See Hourly';
   button.addEventListener('click', () => {
-    clearCards();
+    let isDoneOnce = false;
+    document
+      .querySelectorAll('.main > .card:not(.header):not(.detail-weather)')
+      .forEach(async (otherCard) => {
+        await fadeOutCard(otherCard);
+        otherCard.classList.add('display-none');
+        while (!isDoneOnce) {
+          const detailCard = document.querySelector('.main > .card.detail-weather.hourly');
+          detailCard.classList.remove('display-none');
+          fadeInCard(detailCard);
+          isDoneOnce = true;
+        }
+      });
+    window.scrollTo(0, 0);
   });
 
   const currentHour = (new Date(response.current.last_updated)).getHours();
@@ -447,6 +460,202 @@ const generateHourlyForecastCard = (response) => {
   mainContainer.appendChild(card);
 };
 
+const generateHourlyWeatherCard = (response) => {
+  const card = document.createElement('div');
+  card.classList.add('detail-weather', 'card', 'hourly', 'display-none');
+
+  const h2 = document.createElement('h2');
+  h2.textContent = 'Hourly Weather';
+
+  const span = document.createElement('span');
+  span.classList.add('last-updated');
+  span.textContent = ` as of ${dateFormat(new Date(response.current.last_updated), 'p')}`;
+
+  const currentLocation = document.createElement('p');
+  currentLocation.classList.add('current-location');
+  currentLocation.textContent = `${response.location.name}, ${response.location.country}`;
+
+  const button = document.createElement('button');
+  button.classList.add('details-button');
+  button.textContent = 'Return to Current Weather';
+  button.addEventListener('click', async () => {
+    await fadeOutCard(card);
+    card.classList.add('display-none');
+    document
+      .querySelectorAll('.main > .card:not(.header):not(.detail-weather)')
+      .forEach(async (otherCard) => {
+        otherCard.classList.remove('display-none');
+        fadeInCard(otherCard);
+      });
+    window.scrollTo(0, 0);
+  });
+
+  h2.appendChild(span);
+  card.appendChild(h2);
+  card.appendChild(currentLocation);
+
+  for (let day = 0; day < 3; day++) {
+    const h3 = document.createElement('h3');
+    h3.textContent = dateFormat(new Date(response.forecast.forecastday[day].date), 'EEEE, MMMM d');
+
+    card.appendChild(h3);
+
+    for (let hour = 0; hour < 24; hour++) {
+      const detailEntry = document.createElement('div');
+      detailEntry.classList.add('detail-entry');
+
+      const hourly = document.createElement('div');
+      hourly.textContent = dateFormat(new Date(response.forecast.forecastday[day].hour[hour].time), 'p');
+
+      const temp = document.createElement('p');
+      temp.classList.add('temp');
+      temp.textContent = `${response.forecast.forecastday[day].hour[hour].temp_c}°`;
+
+      const condition = document.createElement('div');
+
+      const conditionImg = document.createElement('img');
+      conditionImg.src = `http://${response.forecast.forecastday[day].hour[hour].condition.icon}`;
+      conditionImg.alt = response.forecast.forecastday[day].hour[hour].condition.text;
+
+      const conditionP = document.createElement('img');
+      conditionP.textContent = response.forecast.forecastday[day].hour[hour].condition.text;
+
+      const precipitation = document.createElement('div');
+
+      const precipitationSVG = document.createElement('svg');
+
+      const precipitationP = document.createElement('p');
+      precipitationP.textContent = `${response.forecast.forecastday[day].hour[hour].chance_of_rain}%`;
+
+      const wind = document.createElement('div');
+
+      const windSVG = document.createElement('svg');
+
+      const windP = document.createElement('p');
+      windP.textContent = `${response.forecast.forecastday[day].hour[hour].wind_kph} kph ${response.forecast.forecastday[day].hour[hour].wind_dir}`;
+
+      const expand = document.createElement('svg');
+
+      // start
+
+      const conditions = document.createElement('div');
+      conditions.classList.add('conditions', 'card', 'display-none');
+
+      const conditionsInput = [
+        {
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>thermometer</title><path d="M15 13V5A3 3 0 0 0 9 5V13A5 5 0 1 0 15 13M12 4A1 1 0 0 1 13 5V8H11V5A1 1 0 0 1 12 4Z"></path></svg>',
+          description: 'Feels like',
+          value: `${response.forecast.forecastday[day].hour[hour].feelslike_c}°`,
+        },
+        {
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>weather-rainy</title><path d="M6,14.03A1,1 0 0,1 7,15.03C7,15.58 6.55,16.03 6,16.03C3.24,16.03 1,13.79 1,11.03C1,8.27 3.24,6.03 6,6.03C7,3.68 9.3,2.03 12,2.03C15.43,2.03 18.24,4.69 18.5,8.06L19,8.03A4,4 0 0,1 23,12.03C23,14.23 21.21,16.03 19,16.03H18C17.45,16.03 17,15.58 17,15.03C17,14.47 17.45,14.03 18,14.03H19A2,2 0 0,0 21,12.03A2,2 0 0,0 19,10.03H17V9.03C17,6.27 14.76,4.03 12,4.03C9.5,4.03 7.45,5.84 7.06,8.21C6.73,8.09 6.37,8.03 6,8.03A3,3 0 0,0 3,11.03A3,3 0 0,0 6,14.03M12,14.15C12.18,14.39 12.37,14.66 12.56,14.94C13,15.56 14,17.03 14,18C14,19.11 13.1,20 12,20A2,2 0 0,1 10,18C10,17.03 11,15.56 11.44,14.94C11.63,14.66 11.82,14.4 12,14.15M12,11.03L11.5,11.59C11.5,11.59 10.65,12.55 9.79,13.81C8.93,15.06 8,16.56 8,18A4,4 0 0,0 12,22A4,4 0 0,0 16,18C16,16.56 15.07,15.06 14.21,13.81C13.35,12.55 12.5,11.59 12.5,11.59"></path></svg>',
+          description: 'Chances of Rain',
+          value: `${response.forecast.forecastday[day].hour[hour].chance_of_rain}%`,
+        },
+        {
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>weather-snowy</title><path d="M6,14A1,1 0 0,1 7,15A1,1 0 0,1 6,16A5,5 0 0,1 1,11A5,5 0 0,1 6,6C7,3.65 9.3,2 12,2C15.43,2 18.24,4.66 18.5,8.03L19,8A4,4 0 0,1 23,12A4,4 0 0,1 19,16H18A1,1 0 0,1 17,15A1,1 0 0,1 18,14H19A2,2 0 0,0 21,12A2,2 0 0,0 19,10H17V9A5,5 0 0,0 12,4C9.5,4 7.45,5.82 7.06,8.19C6.73,8.07 6.37,8 6,8A3,3 0 0,0 3,11A3,3 0 0,0 6,14M7.88,18.07L10.07,17.5L8.46,15.88C8.07,15.5 8.07,14.86 8.46,14.46C8.85,14.07 9.5,14.07 9.88,14.46L11.5,16.07L12.07,13.88C12.21,13.34 12.76,13.03 13.29,13.17C13.83,13.31 14.14,13.86 14,14.4L13.41,16.59L15.6,16C16.14,15.86 16.69,16.17 16.83,16.71C16.97,17.24 16.66,17.79 16.12,17.93L13.93,18.5L15.54,20.12C15.93,20.5 15.93,21.15 15.54,21.54C15.15,21.93 14.5,21.93 14.12,21.54L12.5,19.93L11.93,22.12C11.79,22.66 11.24,22.97 10.71,22.83C10.17,22.69 9.86,22.14 10,21.6L10.59,19.41L8.4,20C7.86,20.14 7.31,19.83 7.17,19.29C7.03,18.76 7.34,18.21 7.88,18.07Z"></path></svg>',
+          description: 'Chances of Snow',
+          value: `${response.forecast.forecastday[day].hour[hour].chance_of_snow}%`,
+        },
+        {
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>weather-windy</title><path d="M4,10A1,1 0 0,1 3,9A1,1 0 0,1 4,8H12A2,2 0 0,0 14,6A2,2 0 0,0 12,4C11.45,4 10.95,4.22 10.59,4.59C10.2,5 9.56,5 9.17,4.59C8.78,4.2 8.78,3.56 9.17,3.17C9.9,2.45 10.9,2 12,2A4,4 0 0,1 16,6A4,4 0 0,1 12,10H4M19,12A1,1 0 0,0 20,11A1,1 0 0,0 19,10C18.72,10 18.47,10.11 18.29,10.29C17.9,10.68 17.27,10.68 16.88,10.29C16.5,9.9 16.5,9.27 16.88,8.88C17.42,8.34 18.17,8 19,8A3,3 0 0,1 22,11A3,3 0 0,1 19,14H5A1,1 0 0,1 4,13A1,1 0 0,1 5,12H19M18,18H4A1,1 0 0,1 3,17A1,1 0 0,1 4,16H18A3,3 0 0,1 21,19A3,3 0 0,1 18,22C17.17,22 16.42,21.66 15.88,21.12C15.5,20.73 15.5,20.1 15.88,19.71C16.27,19.32 16.9,19.32 17.29,19.71C17.47,19.89 17.72,20 18,20A1,1 0 0,0 19,19A1,1 0 0,0 18,18Z"></path></svg>',
+          description: 'Wind',
+          value: `${response.forecast.forecastday[day].hour[hour].wind_kph} kph ${response.forecast.forecastday[0].hour[hour].wind_dir}`,
+        },
+        {
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>water-circle</title><path d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M12 19C9.24 19 7 16.76 7 14C7 10.67 12 5.04 12 5.04S17 10.67 17 14C17 16.76 14.76 19 12 19Z"></path></svg>',
+          description: 'Humidity',
+          value: `${response.forecast.forecastday[day].hour[hour].humidity}%`,
+        },
+        {
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>format-vertical-align-center</title><path d="M8,19H11V23H13V19H16L12,15L8,19M16,5H13V1H11V5H8L12,9L16,5M4,11V13H20V11H4Z"></path></svg>',
+          description: 'Pressure',
+          value: `${response.forecast.forecastday[day].hour[hour].pressure_mb} mb`,
+        },
+      ];
+
+      const content = document.createElement('div');
+      content.classList.add('content');
+
+      const content1 = document.createElement('div');
+      const content2 = document.createElement('div');
+
+      conditionsInput.forEach((input, index) => {
+        const conditionEntry = document.createElement('div');
+        conditionEntry.classList.add('condition-entry');
+
+        const svg = document.createElement('div');
+
+        const description = document.createElement('p');
+        description.textContent = input.description;
+
+        const value = document.createElement('p');
+        value.textContent = input.value;
+
+        conditionEntry.appendChild(svg);
+        conditionEntry.appendChild(description);
+        conditionEntry.appendChild(value);
+
+        if (index < 3) {
+          content1.appendChild(conditionEntry);
+        } else if ((index < 6) && (index >= 3)) {
+          content2.appendChild(conditionEntry);
+        }
+
+        svg.outerHTML = input.svg;
+      });
+
+      // end
+
+      detailEntry.addEventListener('click', () => {
+        const expand = document.querySelector(`.expand-${day}-${hour}`);
+        if (conditions.classList.contains('display-none')) {
+          expand.outerHTML = `<svg class="expand-${day}-${hour}"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>chevron-up</title><path d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z" /></svg>`;
+          conditions.classList.remove('display-none');
+        } else {
+          expand.outerHTML = `<svg class="expand-${day}-${hour}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>chevron-down</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"></path></svg>`;
+          conditions.classList.add('display-none');
+        }
+        fadeInCard(conditions);
+      });
+
+      button.addEventListener('click', () => {
+        setTimeout(() => {
+          const expand = document.querySelector(`.expand-${day}-${hour}`);
+          expand.outerHTML = `<svg class="expand-${day}-${hour}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>chevron-down</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"></path></svg>`;
+          conditions.classList.add('display-none');
+          fadeInCard(conditions);
+        }, 500);
+      });
+
+      content.appendChild(content1);
+      content.appendChild(content2);
+      conditions.appendChild(content);
+      condition.appendChild(conditionImg);
+      condition.appendChild(conditionP);
+      precipitation.appendChild(precipitationSVG);
+      precipitation.appendChild(precipitationP);
+      wind.appendChild(windSVG);
+      wind.appendChild(windP);
+      detailEntry.appendChild(hourly);
+      detailEntry.appendChild(temp);
+      detailEntry.appendChild(condition);
+      detailEntry.appendChild(precipitation);
+      detailEntry.appendChild(wind);
+      detailEntry.appendChild(expand);
+      detailEntry.appendChild(conditions);
+      card.appendChild(detailEntry);
+
+      precipitationSVG.outerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>water</title><path d="M12,20A6,6 0 0,1 6,14C6,10 12,3.25 12,3.25C12,3.25 18,10 18,14A6,6 0 0,1 12,20Z"></path></svg>';
+      windSVG.outerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>weather-windy</title><path d="M4,10A1,1 0 0,1 3,9A1,1 0 0,1 4,8H12A2,2 0 0,0 14,6A2,2 0 0,0 12,4C11.45,4 10.95,4.22 10.59,4.59C10.2,5 9.56,5 9.17,4.59C8.78,4.2 8.78,3.56 9.17,3.17C9.9,2.45 10.9,2 12,2A4,4 0 0,1 16,6A4,4 0 0,1 12,10H4M19,12A1,1 0 0,0 20,11A1,1 0 0,0 19,10C18.72,10 18.47,10.11 18.29,10.29C17.9,10.68 17.27,10.68 16.88,10.29C16.5,9.9 16.5,9.27 16.88,8.88C17.42,8.34 18.17,8 19,8A3,3 0 0,1 22,11A3,3 0 0,1 19,14H5A1,1 0 0,1 4,13A1,1 0 0,1 5,12H19M18,18H4A1,1 0 0,1 3,17A1,1 0 0,1 4,16H18A3,3 0 0,1 21,19A3,3 0 0,1 18,22C17.17,22 16.42,21.66 15.88,21.12C15.5,20.73 15.5,20.1 15.88,19.71C16.27,19.32 16.9,19.32 17.29,19.71C17.47,19.89 17.72,20 18,20A1,1 0 0,0 19,19A1,1 0 0,0 18,18Z"></path></svg>';
+      expand.outerHTML = `<svg class="expand-${day}-${hour}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>chevron-down</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"></path></svg>`;
+    }
+  }
+
+  card.appendChild(button);
+  mainContainer.appendChild(card);
+};
+
 const generateDailyForecastCard = (response) => {
   const card = document.createElement('div');
   card.classList.add('forecast', 'card', 'hidden');
@@ -463,7 +672,7 @@ const generateDailyForecastCard = (response) => {
   button.addEventListener('click', () => {
     let isDoneOnce = false;
     document
-      .querySelectorAll('.main > .card:not(.header):not(.detail-weather.daily)')
+      .querySelectorAll('.main > .card:not(.header):not(.detail-weather)')
       .forEach(async (otherCard) => {
         await fadeOutCard(otherCard);
         otherCard.classList.add('display-none');
@@ -474,6 +683,7 @@ const generateDailyForecastCard = (response) => {
           isDoneOnce = true;
         }
       });
+    window.scrollTo(0, 0);
   });
 
   const forecastInput = [];
@@ -554,11 +764,12 @@ const generateDailyWeatherCard = (response) => {
     await fadeOutCard(card);
     card.classList.add('display-none');
     document
-      .querySelectorAll('.main > .card:not(.header):not(.detail-weather.daily)')
+      .querySelectorAll('.main > .card:not(.header):not(.detail-weather)')
       .forEach(async (otherCard) => {
         otherCard.classList.remove('display-none');
         fadeInCard(otherCard);
       });
+    window.scrollTo(0, 0);
   });
 
   h2.appendChild(span);
@@ -945,6 +1156,7 @@ const generateCards = (response) => {
   generateTodaysForecastCard(response);
   generateHourlyForecastCard(response);
   generateDailyForecastCard(response);
+  generateHourlyWeatherCard(response);
   generateDailyWeatherCard(response);
   fadeInCards();
 };
